7ac0a94e808746357d06b52b8aa3c40c0e92330d
RishubPatel <rpate133@calpoly.edu>
2024-11-08 08:47:34 -0800

    Implemented parts 1-3

--- convert.py
+++ convert.py
@@ -1,1 +1,9 @@
+from typing import Optional
 
+def str_to_float(string: str) -> Optional[float]: #Converts a given string to a float if possible; else returns None
+    try:
+        return float(string)
+    except ValueError:
+        return None
+    except:
+        return None
--- keyboard.py
+++ keyboard.py
@@ -1,1 +1,14 @@
+import convert
 
+def gather_numbers() -> list[float]: #reads a series of inputted numbers
+    nums = []
+    while True:
+        num = input("Enter a number NOW.\t")
+        if num == "done":
+            return nums
+        num_processed = convert.str_to_float(num)
+        if num_processed != None:
+            nums += [num_processed]
+
+if __name__ == "__main__":
+    print(sum(gather_numbers()))
